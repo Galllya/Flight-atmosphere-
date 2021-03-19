df03736aa8bcaaa42ec6a119871c26ceb4e4074f
Galya Bolganova <70851177+Galllya@users.noreply.github.com>
2021-03-19 18:09:52 +0700

    main

---
+++ Flight/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+   
+
+      
+
+        Formuls furm = new Formuls();
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            furm.makeValue((double)edAngle.Value, (double)edSpeed.Value, (double)edHeight.Value, (double)edWeight.Value,
+               (double)edSquare.Value);
+
+            furm.Position();
+
+           
+            
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(furm.x, furm.y);
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            furm.main();
+
+            chart1.Series[0].Points.AddXY(furm.x, furm.y);
+            if (furm.y <= 0) timer1.Stop();
+        }
+
+        private void edAngle_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Flight/Formuls.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight
+{
+    public class Formuls
+    {
+
+        const double dt = 0.1;
+        const double g = 9.81;
+        const double C = 0.15;
+        const double rho = 1.29;
+
+        public double a;
+        public double v0;
+        public double y0;
+        public double m;
+        public double S;
+        public double k;
+
+        public double t;
+        public double x;
+        public double y;
+        public double vx;
+        public double vy;
+
+        public void makeValue(double edAngle, double edSpeed, double edHeight, double edWeight, double edSquare)
+        {
+            a = edAngle; 
+            v0 = edSpeed;
+            y0 = edHeight;
+            m = edWeight;
+            S = edSquare;
+            k = 0.5 * C * S * rho / m;
+        }
+
+        public void Position()
+        {
+            vx = v0 * Math.Cos(a * Math.PI / 180);
+            vy = v0 * Math.Sin(a * Math.PI / 180);
+            t = 0;
+            x = 0;
+            y = y0;
+        }
+       
+        public void main()
+        {
+            t += dt;
+            vx = vx - k * vx * Math.Sqrt(vx * vx + vy * vy) * dt;
+            vy = vy - (g + k * vy * Math.Sqrt(vx * vx + vy * vy)) * dt;
+
+            x = x + vx * dt;
+            y = y + vy * dt;
+        }
+
+    }
+}
